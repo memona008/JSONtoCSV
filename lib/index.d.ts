cf7decde304458f09033b0cee5c43a883a1b8d6c
MemonaSultan <memonasultan54@gmail.com>
2019-09-26 11:02:38 -0700

    Initial Commit

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+interface IMappedColumns {
+    key: string;
+    value: string;
+}
+export declare let _DownloadJsonData: (JSONData: any, FileTitle: string, fileNameMode: number | null, ShowLabel: boolean, MappedColumn: IMappedColumns[]) => void;
+export {};
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+
+
+interface IMappedColumns {
+    key:string;
+    value:string;
+
+}
+
+
+export let _DownloadJsonData = (JSONData: any, FileTitle: string,fileNameMode:number|null, ShowLabel: boolean, MappedColumn: IMappedColumns[]): void => {
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    var CSV = '';
+    if (ShowLabel) {
+      var row = '';
+      for (var index in arrData[0]) {
+        var filter = MappedColumn.filter((a) => a.key == index);
+        if (filter && filter.length > 0) {
+          row += filter[0].value + ',';
+        } else {
+          row += index + ',';
+        }
+      }
+      row = row.slice(0, -1);
+      CSV += row + '\r\n';
+    }
+    for (var i = 0; i < arrData.length; i++) {
+      var rowi = '';
+      for (var indexs in arrData[i]) {
+        rowi += '"' + arrData[i][indexs] + '",';
+      }
+      rowi.slice(0, rowi.length - 1);
+      CSV += rowi + '\r\n';
+    }
+    if (CSV == '') {
+      alert('Invalid data');
+      return;
+    }
+    var filename = FileTitle;;
+    switch(fileNameMode)
+    {
+        case 1: 
+        filename = FileTitle;
+        break;
+        case 2: 
+        filename = FileTitle + '_' + new Date();
+        break;
+        default: 
+        filename = FileTitle;
+        
+    } 
+    var blob = new Blob([CSV], {
+      type: 'text/csv;charset=utf-8;',
+    });
+    if (navigator.msSaveBlob) {
+      // IE 10+
+      navigator.msSaveBlob(blob, filename + '.csv');
+    } else {
+      var link = document.createElement('a');
+      if (link.download !== undefined) {
+        var url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.hidden = true;
+        link.download = filename + '.csv';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
+  };
